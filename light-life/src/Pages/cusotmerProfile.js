0bc53a52bb8d720a2e27876554eaca487e1af2e1
NycteaChen <jungturn01tw@gmail.com>
2021-05-15 22:08:37 +0800

    customer profile editing & saving switch

--- light-life/src/Pages/cusotmerProfile.js
+++ light-life/src/Pages/cusotmerProfile.js
@@ -11,10 +11,11 @@ import {
   useLocation,
 } from "react-router-dom";
 
-function CustomerProfile({ props }) {
+function EditCustomerProfile({ props }) {
   const db = firebase.firestore();
   const storage = firebase.storage();
   const [input, setInput] = useState({});
+  const [isEditing, setIsEditing] = useState(false);
   const {
     name,
     image,
@@ -76,7 +77,6 @@ function CustomerProfile({ props }) {
   };
 
   const bindSaveHandler = async () => {
-    // delete input.previewImg;
     if (input.imageFile) {
       const imageUrl = await getImg(input.imageFile);
       delete input.imageFile;
@@ -95,283 +95,306 @@ function CustomerProfile({ props }) {
     } else {
       db.collection("customers").doc("9iYZMkuFdZRK9vxgt1zc").update(input);
     }
+    setIsEditing(false);
   };
+  const bindEditHandler = () => {
+    setIsEditing(true);
+  };
+
   return (
-    <>
-      <div id="customer-profile">
-        <div>客戶資料</div>
-        <div>
-          <img
-            src={
-              input.previewImg
-                ? input.previewImg
-                : input.image
-                ? input.image
-                : image
-                ? image
-                : noImage
-            }
-            alt="customer"
-            style={{ width: "200px", height: "200px", borderRadius: "50%" }}
-          />
+    <div id="customer-profile">
+      {isEditing ? (
+        <>
           <div>
-            <label>上傳大頭照</label>
-            <input
-              type="file"
-              accept="image/*"
-              id="image"
-              name="image"
-              onChange={setInputHandler}
-            />
-          </div>
-        </div>
-        <div>
-          姓名
-          <input
-            placeholder="姓名"
-            name="name"
-            type="text"
-            id="name"
-            value={input.name ? input.name : name ? name : ""}
-            onChange={setInputHandler}
-          />
-        </div>
-        <div>
-          <div>
-            系統編號<span id="number">{id}</span>
-          </div>
-        </div>
-        <div>
-          <label>性別 </label>
-          <label>
-            <input
-              type="radio"
-              name="gender"
-              value="男"
-              checked={
-                input.gender
-                  ? input.gender === "男"
-                    ? true
-                    : false
-                  : gender === "男"
-                  ? true
-                  : false
+            <div>個人資料</div>
+            <img
+              src={
+                input.previewImg
+                  ? input.previewImg
+                  : input.image
+                  ? input.image
+                  : image
+                  ? image
+                  : noImage
               }
-              onChange={setInputHandler}
+              alt="customer"
+              style={{ width: "200px", height: "200px", borderRadius: "50%" }}
             />
-            男
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="gender"
-              value="女"
-              checked={
-                input.gender
-                  ? input.gender === "女"
-                    ? true
-                    : false
-                  : gender === "女"
-                  ? true
-                  : false
-              }
-              onChange={setInputHandler}
-            />
-            女
-          </label>
-        </div>
-        <div>
-          <label>
-            年齡{" "}
-            <input
-              type="number"
-              name="age"
-              value={input.age ? input.age : age ? age : ""}
-              onChange={setInputHandler}
-            />
-            歲
-          </label>
-        </div>
-        <div>
-          <label>
-            身高{" "}
-            <input
-              type="number"
-              name="height"
-              value={input.height ? input.height : height ? height : ""}
-              onChange={setInputHandler}
-            />
-            cm
-          </label>
-        </div>
-        <div>
-          <label>
-            體重{" "}
-            <input
-              type="number"
-              name="weight"
-              value={input.weight ? input.weight : weight ? weight : ""}
-              onChange={setInputHandler}
-            />
-            kg
-          </label>
-        </div>
-        <div>
-          <label>教育程度</label>
-          <label>
-            <input
-              type="radio"
-              name="education"
-              value="國小"
-              checked={
-                input.education
-                  ? input.education === "國小"
-                    ? true
-                    : false
-                  : education === "國小"
-                  ? true
-                  : false
-              }
-              onChange={setInputHandler}
-            />
-            國小
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="education"
-              value="國中"
-              checked={
-                input.education
-                  ? input.education === "國中"
-                    ? true
-                    : false
-                  : education === "國中"
-                  ? true
-                  : false
-              }
-              onChange={setInputHandler}
-            />
-            國中
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="education"
-              value="高中職"
-              checked={
-                input.education
-                  ? input.education === "高中職"
-                    ? true
-                    : false
-                  : education === "高中職"
-                  ? true
-                  : false
-              }
-              onChange={setInputHandler}
-            />
-            高中職
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="education"
-              value="大專院校"
-              checked={
-                input.education
-                  ? input.education === "大專院校"
-                    ? true
-                    : false
-                  : education === "大專院校"
-                  ? true
-                  : false
-              }
-              onChange={setInputHandler}
-            />
-            大專院校
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="education"
-              value="研究所"
-              checked={
-                input.education
-                  ? input.education === "研究所"
-                    ? true
-                    : false
-                  : education === "研究所"
-                  ? true
-                  : false
-              }
-              onChange={setInputHandler}
-            />
-            研究所
-          </label>
-        </div>
-        <label>
-          職業
-          <select
-            name="career"
-            value={input.career ? input.career : career ? career : ""}
-            onChange={setInputHandler}
-          >
-            <option>軍公教</option>
-            <option>工</option>
-            <option>農</option>
-            <option>商</option>
-            <option>服務業</option>
-            <option>自由業</option>
-            <option>家管</option>
-            <option>其他</option>
-          </select>
-        </label>
-        <div>
-          <label>
-            運動習慣（請大致描述您的運動習慣）{" "}
             <div>
+              <label>上傳大頭照</label>
               <input
-                type="textarea"
-                name="sport"
-                value={input.sport ? input.sport : sport ? sport : ""}
+                type="file"
+                accept="image/*"
+                id="image"
+                name="image"
                 onChange={setInputHandler}
               />
             </div>
-          </label>
-        </div>
-        <div>
-          <label>
-            其他（例：自身狀況、特別需求）{" "}
+          </div>
+          <div>
+            姓名
+            <input
+              placeholder="姓名"
+              name="name"
+              type="text"
+              id="name"
+              value={input.name ? input.name : name ? name : ""}
+              onChange={setInputHandler}
+            />
+          </div>
+          <div>
             <div>
+              系統編號<span id="number">{id}</span>
+            </div>
+          </div>
+          <div>
+            <label>性別 </label>
+            <label>
               <input
-                type="textarea"
-                name="other"
-                value={input.other ? input.other : other ? other : ""}
+                type="radio"
+                name="gender"
+                value="男"
+                checked={
+                  input.gender
+                    ? input.gender === "男"
+                      ? true
+                      : false
+                    : gender === "男"
+                    ? true
+                    : false
+                }
                 onChange={setInputHandler}
               />
-            </div>
+              男
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="gender"
+                value="女"
+                checked={
+                  input.gender
+                    ? input.gender === "女"
+                      ? true
+                      : false
+                    : gender === "女"
+                    ? true
+                    : false
+                }
+                onChange={setInputHandler}
+              />
+              女
+            </label>
+          </div>
+          <div>
+            <label>
+              年齡{" "}
+              <input
+                type="number"
+                name="age"
+                value={input.age ? input.age : age ? age : ""}
+                onChange={setInputHandler}
+              />
+              歲
+            </label>
+          </div>
+          <div>
+            <label>
+              身高{" "}
+              <input
+                type="number"
+                name="height"
+                value={input.height ? input.height : height ? height : ""}
+                onChange={setInputHandler}
+              />
+              cm
+            </label>
+          </div>
+          <div>
+            <label>
+              體重{" "}
+              <input
+                type="number"
+                name="weight"
+                value={input.weight ? input.weight : weight ? weight : ""}
+                onChange={setInputHandler}
+              />
+              kg
+            </label>
+          </div>
+          <div>
+            <label>教育程度</label>
+            <label>
+              <input
+                type="radio"
+                name="education"
+                value="國小"
+                checked={
+                  input.education
+                    ? input.education === "國小"
+                      ? true
+                      : false
+                    : education === "國小"
+                    ? true
+                    : false
+                }
+                onChange={setInputHandler}
+              />
+              國小
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="education"
+                value="國中"
+                checked={
+                  input.education
+                    ? input.education === "國中"
+                      ? true
+                      : false
+                    : education === "國中"
+                    ? true
+                    : false
+                }
+                onChange={setInputHandler}
+              />
+              國中
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="education"
+                value="高中職"
+                checked={
+                  input.education
+                    ? input.education === "高中職"
+                      ? true
+                      : false
+                    : education === "高中職"
+                    ? true
+                    : false
+                }
+                onChange={setInputHandler}
+              />
+              高中職
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="education"
+                value="大專院校"
+                checked={
+                  input.education
+                    ? input.education === "大專院校"
+                      ? true
+                      : false
+                    : education === "大專院校"
+                    ? true
+                    : false
+                }
+                onChange={setInputHandler}
+              />
+              大專院校
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="education"
+                value="研究所"
+                checked={
+                  input.education
+                    ? input.education === "研究所"
+                      ? true
+                      : false
+                    : education === "研究所"
+                    ? true
+                    : false
+                }
+                onChange={setInputHandler}
+              />
+              研究所
+            </label>
+          </div>
+          <label>
+            職業
+            <select
+              name="career"
+              value={input.career ? input.career : career ? career : ""}
+              onChange={setInputHandler}
+            >
+              <option>軍公教</option>
+              <option>工</option>
+              <option>農</option>
+              <option>商</option>
+              <option>服務業</option>
+              <option>自由業</option>
+              <option>家管</option>
+              <option>其他</option>
+            </select>
           </label>
-        </div>
-        <button onClick={bindSaveHandler}>儲存</button>
-      </div>
-    </>
+          <div>
+            <label>
+              運動習慣（請大致描述您的運動習慣）{" "}
+              <div>
+                <input
+                  type="textarea"
+                  name="sport"
+                  value={input.sport ? input.sport : sport ? sport : ""}
+                  onChange={setInputHandler}
+                />
+              </div>
+            </label>
+          </div>
+          <div>
+            <label>
+              其他（例：自身狀況、特別需求）{" "}
+              <div>
+                <input
+                  type="textarea"
+                  name="other"
+                  value={input.other ? input.other : other ? other : ""}
+                  onChange={setInputHandler}
+                />
+              </div>
+            </label>
+          </div>
+          <button onClick={bindSaveHandler}>儲存</button>
+        </>
+      ) : (
+        <>
+          <button onClick={bindEditHandler}>編輯</button>
+          <CustomerProfilte props={props} input={input} />
+        </>
+      )}
+    </div>
   );
 }
 
-function DietitianCustomerProfilte({ props }) {
+function CustomerProfilte({ props, input }) {
   return (
     <div id="personal-profile">
       <div>個人資料</div>
       <div>
         <img
           id="profile-img"
-          src={props ? props.image : noImage}
+          src={
+            input.previewImg
+              ? input.previewImg
+              : input.image
+              ? input.image
+              : props
+              ? props.image
+              : noImage
+          }
           alt="customer"
           style={{ width: "200px", height: "200px", borderRadius: "50%" }}
         />
       </div>
       <div>
-        姓名<span id="name">{props ? props.name : ""}</span>
+        姓名
+        <span id="name">
+          {input.name ? input.name : props ? props.name : ""}
+        </span>
       </div>
       <div>
         <div>
@@ -380,23 +403,38 @@ function DietitianCustomerProfilte({ props }) {
       </div>
       <div>
         <div>
-          性別 <span id="gender">{props ? props.gender : ""}</span>
+          性別{" "}
+          <span id="gender">
+            {input.gender ? input.gender : props ? props.gender : ""}
+          </span>
         </div>
       </div>
       <div>
         <div>
-          年齡 <span id="age">{props ? props.age : ""}</span>
+          年齡{" "}
+          <span id="age">{input.age ? input.age : props ? props.age : ""}</span>
         </div>
         <div>
-          身高 <span id="height">{props ? props.height : ""}</span> cm
+          身高{" "}
+          <span id="height">
+            {input.height ? input.height : props ? props.height : ""}
+          </span>{" "}
+          cm
         </div>
         <div>
-          體重 <span id="weight">{props ? props.weight : ""}</span> kg{" "}
+          體重{" "}
+          <span id="weight">
+            {input.weight ? input.weight : props ? props.weight : ""}
+          </span>{" "}
+          kg{" "}
         </div>
       </div>
       <div>
         <div>
-          教育程度 <span id="education">{props ? props.education : ""}</span>
+          教育程度{" "}
+          <span id="education">
+            {input.education ? input.education : props ? props.education : ""}
+          </span>
         </div>
       </div>
       <div>
@@ -406,11 +444,15 @@ function DietitianCustomerProfilte({ props }) {
       </div>
       <div>
         <div>運動習慣</div>
-        <div id="sport">{props ? props.sport : ""}</div>
+        <div id="sport">
+          {input.career ? input.career : props ? props.sport : ""}
+        </div>
       </div>
       <div>
         <div>其他（例：自身狀況、特別需求）</div>
-        <div id="other">{props ? props.other : ""}</div>
+        <div id="other">
+          {input.other ? input.other : props ? props.other : ""}
+        </div>
       </div>
     </div>
   );
@@ -420,9 +462,9 @@ function RenderCustomerProfile({ profileData }) {
   const pathName = useLocation().pathname;
 
   if (pathName.includes("dietitian")) {
-    return <DietitianCustomerProfilte props={profileData} />;
+    return <CustomerProfilte props={profileData} />;
   } else {
-    return <CustomerProfile props={profileData} />;
+    return <EditCustomerProfile props={profileData} />;
   }
 }
 
