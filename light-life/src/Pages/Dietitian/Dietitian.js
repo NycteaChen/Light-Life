a10286207029510cc06991362516b211115ec893
NycteaChen <jungturn01tw@gmail.com>
2021-05-25 18:50:07 +0800

    modified target editing bug

--- light-life/src/Pages/Dietitian/Dietitian.js
+++ light-life/src/Pages/Dietitian/Dietitian.js
@@ -79,27 +79,123 @@ function Dietitian() {
 
   if (users.length > 0) {
     return (
-      <main className={basic["d-main"]}>
-        <nav>
-          <a href="/">
-            <img src={logo} id={basic["menu-logo"]} />
-          </a>
-          <div className={basic["straight-nav"]}>
-            <Link
-              className={basic["nav-title"]}
-              to={`/dietitian/${dietitianID}/profile`}
-            >
-              <div onClick={bindListHandler}>編輯會員資料</div>
-            </Link>
-            <ul>
-              <div
-                className={`${basic["nav-title"]} list`}
+      <>
+        <main className={basic["d-main"]}>
+          <nav>
+            <a href="/">
+              <img src={logo} id={basic["menu-logo"]} />
+            </a>
+            <div className={basic["straight-nav"]}>
+              <Link
+                className={basic["nav-title"]}
+                to={`/dietitian/${dietitianID}/profile`}
+              >
+                <div onClick={bindListHandler}>編輯會員資料</div>
+              </Link>
+              <ul>
+                <div
+                  className={`${basic["nav-title"]} list`}
+                  onClick={bindListHandler}
+                >
+                  客戶清單
+                </div>
+                <div
+                  className={`${basic.customerList} list`}
+                  style={{ display: display }}
+                >
+                  {users.map((c, index) => (
+                    <li
+                      key={index}
+                      className={c.id}
+                      onClick={getSelectedCustomer}
+                    >
+                      <Link
+                        to={`/dietitian/${c.dietitian}/customer/${c.id}`}
+                        className={c.id}
+                        style={{ fontSize: "16px" }}
+                      >
+                        {c.name}
+                      </Link>
+                    </li>
+                  ))}
+                </div>
+              </ul>
+              <div className={basic["nav-title"]} onClick={bindListHandler}>
+                找客戶
+              </div>
+              <Link
+                className={basic["nav-title"]}
+                to={`/dietitian/${dietitianID}/inviteMe`}
+              >
+                <div onClick={bindListHandler}>誰找我</div>
+              </Link>
+              <Link
+                className={basic["nav-title"]}
+                onClick={bindListHandler}
+                to={`/dietitian/${dietitianID}`}
+              >
+                返回會員主頁
+              </Link>
+              <a href="/">
+                <img src={exit} alt="logout" id={basic.logout} />
+              </a>
+              <div className={basic.copyright}>&copy;2021 Light Life</div>
+            </div>
+          </nav>
+
+          <div className={basic.profile}>
+            <img src={profile ? profile.image : noImage} />
+            <div className={basic.welcome}>
+              <div>
+                <span>{profile ? profile.name : ""}</span>，您好
+              </div>
+              <div className={basic["service-status"]}>
+                <div>服務開放中</div>
+                <label>
+                  <input type="checkbox" />
+                </label>
+              </div>
+            </div>
+            <div className={basic.selectList}>
+              <Link
+                className="list"
+                to={`/dietitian/${dietitianID}/profile`}
                 onClick={bindListHandler}
               >
-                客戶清單
+                <div>編輯會員資料</div>
+              </Link>
+              <Link to="#" onClick={bindListHandler}>
+                <div>找客戶</div>
+              </Link>
+              <Link
+                className="list"
+                to={`/dietitian/${dietitianID}/customers`}
+                onClick={bindListHandler}
+              >
+                <div>客戶清單</div>
+              </Link>
+
+              <Link
+                to={`/dietitian/${dietitianID}/inviteMe`}
+                onClick={bindListHandler}
+              >
+                <div> 誰找我</div>
+              </Link>
+            </div>
+          </div>
+
+          <Switch>
+            <Route exact path="/dietitian/:dID">
+              <div className={basic.indexWelcome}>
+                {profile.name}營養師，歡迎回來！
               </div>
+            </Route>
+            <Route exact path={`/dietitian/:dID/profile`}>
+              <DietitianProfile profile={profile} />
+            </Route>
+            <Route exact path={`/dietitian/:dID/customers`}>
               <div
-                className={`${basic.customerList} list`}
+                className={basic.mobileCustomerList}
                 style={{ display: display }}
               >
                 {users.map((c, index) => (
@@ -111,174 +207,87 @@ function Dietitian() {
                     <Link
                       to={`/dietitian/${c.dietitian}/customer/${c.id}`}
                       className={c.id}
-                      style={{ fontSize: "16px" }}
                     >
                       {c.name}
                     </Link>
                   </li>
                 ))}
               </div>
-            </ul>
-            <div className={basic["nav-title"]} onClick={bindListHandler}>
-              找客戶
-            </div>
-            <Link
-              className={basic["nav-title"]}
-              to={`/dietitian/${dietitianID}/inviteMe`}
-            >
-              <div onClick={bindListHandler}>誰找我</div>
-            </Link>
-            <Link
-              className={basic["nav-title"]}
-              onClick={bindListHandler}
-              to={`/dietitian/${dietitianID}`}
-            >
-              返回會員主頁
-            </Link>
-            <a href="/">
-              <img src={exit} alt="logout" id={basic.logout} />
-            </a>
-            <div className={basic.copyright}>&copy;2021 Light Life</div>
-          </div>
-        </nav>
-
-        <div className={basic.profile}>
-          <img src={profile ? profile.image : noImage} />
-          <div className={basic.welcome}>
-            <div>
-              <span>{profile ? profile.name : ""}</span>，您好
-            </div>
-            <div className={basic["service-status"]}>
-              <div>服務開放中</div>
-              <label>
-                <input type="checkbox" />
-              </label>
-            </div>
-          </div>
-          <div className={basic.selectList}>
-            <div>
-              <Link
-                to={`/dietitian/${dietitianID}/profile`}
-                onClick={bindListHandler}
-              >
-                編輯會員資料
-              </Link>
-            </div>
-            <div onClick={bindListHandler}>找客戶</div>
-            <div className="list">
-              <Link
-                className="list"
-                to={`/dietitian/${dietitianID}/customers`}
-                onClick={bindListHandler}
-              >
-                客戶清單
-              </Link>
-            </div>
-            <div onClick={bindListHandler}>
-              <Link to={`/dietitian/${dietitianID}/inviteMe`}>誰找我 </Link>
-            </div>
-          </div>
-        </div>
-
-        <Switch>
-          <Route exact path="/dietitian/:dID">
-            <div className={basic.indexWelcome}>
-              {profile.name}營養師，歡迎回來！
-            </div>
-          </Route>
-          <Route exact path={`/dietitian/:dID/profile`}>
-            <DietitianProfile profile={profile} />
-          </Route>
-          <Route exact path={`/dietitian/:dID/customers`}>
-            <div
-              className={basic.mobileCustomerList}
-              style={{ display: display }}
-            >
-              {users.map((c, index) => (
-                <li key={index} className={c.id} onClick={getSelectedCustomer}>
-                  <Link
-                    to={`/dietitian/${c.dietitian}/customer/${c.id}`}
-                    className={c.id}
-                  >
-                    {c.name}
-                  </Link>
-                </li>
-              ))}
-            </div>
-          </Route>
-          <Route exact path={`/dietitian/:dID/inviteMe`}>
-            <div>
-              <InvitedList
-                invitedList={invitedList}
-                setInvitedList={setInvitedList}
-              />
-            </div>
-          </Route>
-        </Switch>
-
-        <Switch>
-          <Route
-            path={`/dietitian/${dietitianID}/customer/${
-              customerID ? customerID : selectedID
-            }`}
-          >
-            <Router>
-              <div className={style["customer-data"]}>
-                <div>
-                  <Link
-                    className={style["customer-name"]}
-                    to={`/dietitian/${dietitianID}/customer/${
-                      customerID ? customerID : selectedID
-                    }`}
-                  >
-                    陳安妮
-                  </Link>
-                </div>
-                <div className={style["customer-dataSelect"]}>
-                  <Link
-                    className={style["link-select"]}
-                    to={`/dietitian/${dietitianID}/customer/${
-                      customerID ? customerID : selectedID
-                    }/profile`}
-                  >
-                    基本資料
-                  </Link>
-                  <Link
-                    className={style["link-select"]}
-                    to={`/dietitian/${dietitianID}/customer/${
-                      customerID ? customerID : selectedID
-                    }/dietary`}
-                  >
-                    飲食記錄
-                  </Link>
-                  <Link
-                    className={style["link-select"]}
-                    to={`/dietitian/${dietitianID}/customer/${
-                      customerID ? customerID : selectedID
-                    }/target`}
-                  >
-                    目標設定
-                  </Link>
-                </div>
+            </Route>
+            <Route exact path={`/dietitian/:dID/inviteMe`}>
+              <div>
+                <InvitedList
+                  invitedList={invitedList}
+                  setInvitedList={setInvitedList}
+                />
               </div>
-              <Switch>
-                <Route exact path={`/dietitian/:dID/customer/:cID/`}>
-                  <div style={{ marginLeft: "300px" }}>服務時間</div>
-                </Route>
-                <Route exact path={`/dietitian/:dID/customer/:cID/profile`}>
-                  <CusotmerProfile props={users[0]} input={input} />
-                </Route>
-                <Route exact path={`/dietitian/:dID/customer/:cID/dietary`}>
-                  <DietrayRecord />
-                </Route>
-                <Route exact path={`/dietitian/:dID/customer/:cID/target`}>
-                  <DietitianTarget />
-                </Route>
-              </Switch>
-            </Router>
-          </Route>
-        </Switch>
-      </main>
+            </Route>
+          </Switch>
+
+          <Switch>
+            <Route
+              path={`/dietitian/${dietitianID}/customer/${
+                customerID ? customerID : selectedID
+              }`}
+            >
+              <Router>
+                <div className={style["customer-data"]}>
+                  <div>
+                    <Link
+                      className={style["customer-name"]}
+                      to={`/dietitian/${dietitianID}/customer/${
+                        customerID ? customerID : selectedID
+                      }`}
+                    >
+                      陳安妮
+                    </Link>
+                  </div>
+                  <div className={style["customer-dataSelect"]}>
+                    <Link
+                      className={style["link-select"]}
+                      to={`/dietitian/${dietitianID}/customer/${
+                        customerID ? customerID : selectedID
+                      }/profile`}
+                    >
+                      基本資料
+                    </Link>
+                    <Link
+                      className={style["link-select"]}
+                      to={`/dietitian/${dietitianID}/customer/${
+                        customerID ? customerID : selectedID
+                      }/dietary`}
+                    >
+                      飲食記錄
+                    </Link>
+                    <Link
+                      className={style["link-select"]}
+                      to={`/dietitian/${dietitianID}/customer/${
+                        customerID ? customerID : selectedID
+                      }/target`}
+                    >
+                      目標設定
+                    </Link>
+                  </div>
+                </div>
+                <Switch>
+                  <Route exact path={`/dietitian/:dID/customer/:cID/`}>
+                    <div style={{ marginLeft: "300px" }}>服務時間</div>
+                  </Route>
+                  <Route exact path={`/dietitian/:dID/customer/:cID/profile`}>
+                    <CusotmerProfile props={users[0]} input={input} />
+                  </Route>
+                  <Route exact path={`/dietitian/:dID/customer/:cID/dietary`}>
+                    <DietrayRecord />
+                  </Route>
+                  <Route exact path={`/dietitian/:dID/customer/:cID/target`}>
+                    <DietitianTarget />
+                  </Route>
+                </Switch>
+              </Router>
+            </Route>
+          </Switch>
+        </main>
+      </>
     );
   } else {
     return (
