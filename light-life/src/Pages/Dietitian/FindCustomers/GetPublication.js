a6eb671e74eb66da99102d2ac37d0d1e5b76910f
NycteaChen <jungturn01tw@gmail.com>
2021-05-29 23:03:00 +0800

    dietitian customer-finding page: inviing & canceling feature

--- light-life/src/Pages/Dietitian/FindCustomers/GetPublication.js
+++ light-life/src/Pages/Dietitian/FindCustomers/GetPublication.js
@@ -11,6 +11,7 @@ import "firebase/firestore";
 import PublicationData from "./PublicationData.js";
 import style from "../../../style/findCustomers.module.scss";
 import { isCompositeComponent } from "react-dom/test-utils";
+import { set } from "date-fns";
 
 function GetPublication() {
   const [publish, setPublish] = useState([]);
@@ -35,31 +36,23 @@ function GetPublication() {
     setIdx(e.target.id);
     setDisplay("block");
   };
-  console.log(publish);
-  const test = () => {
-    // const b = publish
-    //   .find((e, index) => e.whoInvite)
-    //   .whoInvite.find((e) => e.dietitianID === dID);
-    // const a = publish.map((p) => p.whoInvite);
-    // const b = a.find((e) => e.dietitianID === dID);
-    // console.log(a);
-    // const b = a.find((e) => e.length > 0).find((e) => e.dietitianID === dID);
-    // console.log(b);
-    // console.log(b);
-    publish.forEach((e, index) => {
-      if (e.whoInvite) {
-        e.whoInvite.forEach((w, idx) => {
-          if (w.dietitianID === dID) {
-            console.log(`第${index}個刊登的publish中`);
-            console.log(w);
-          }
-        });
-      }
-    });
+  const cancelInviteHandler = (e) => {
+    const { publishID, whoInvite } = publish[parseInt(e.target.id)];
+    firebase
+      .firestore()
+      .collection("publish")
+      .doc(publishID)
+      .set(
+        {
+          whoInvite: [...whoInvite.filter((i) => i.dietitianID !== dID)],
+        },
+        { merge: true }
+      )
+      .then(() => {
+        alert("取消成功!");
+        window.location.reload();
+      });
   };
-  if (publish) {
-    test();
-  }
 
   return (
     <>
@@ -67,67 +60,120 @@ function GetPublication() {
         <h3>刊登中</h3>
 
         {publish
-          ? publish.map((p, index) => (
-              <>
-                <div className={style.publicationList} key={index}>
-                  <div className={style.publication}>
-                    <div className={style.col}>
-                      <div className={style.flexbox}>
+          ? publish.map((p, pubIndex) =>
+              !p.whoInvite ||
+              (p.whoInvite &&
+                !p.whoInvite.find((e) => e.dietitianID === dID)) ? (
+                <>
+                  <div className={style.publicationList} key={pubIndex}>
+                    <div className={style.publication}>
+                      <div className={style.col}>
                         <div className={style.flexbox}>
-                          <div>預約時間</div>
-                          <div>
-                            {p.startDate}~{p.endDate}
+                          <div className={style.flexbox}>
+                            <div>預約時間</div>
+                            <div>
+                              {p.startDate}~{p.endDate}
+                            </div>
+                          </div>
+                          <div className={style.name}>
+                            {p.name} {p.gender === "男" ? "先生" : "小姐"}
                           </div>
                         </div>
-                        <div className={style.name}>
-                          {p.name} {p.gender === "男" ? "先生" : "小姐"}
-                        </div>
+                        <div className={style.subject}>主旨：{p.subject}</div>
                       </div>
-                      <div className={style.subject}>主旨：{p.subject}</div>
+                      <button id={pubIndex} onClick={checkDetailsHandler}>
+                        查看詳情
+                      </button>
                     </div>
-                    <button id={index} onClick={checkDetailsHandler}>
-                      查看詳情
-                    </button>
                   </div>
-                </div>
-                {parseInt(idx) == index ? (
-                  <PublicationData
-                    key={p.name}
-                    publish={p}
-                    display={display}
-                    setDisplay={setDisplay}
-                  />
-                ) : (
-                  ""
-                )}
-              </>
-            ))
+                  {parseInt(idx) == pubIndex ? (
+                    <PublicationData
+                      key={p.name}
+                      publish={p}
+                      display={display}
+                      setDisplay={setDisplay}
+                    />
+                  ) : (
+                    ""
+                  )}
+                </>
+              ) : (
+                ""
+              )
+            )
           : ""}
       </div>
 
       <div className={style["inviting-status"]}>
         <h3>邀請狀態</h3>
-        {publish.map((p, index) =>
-          p.whoInvite ? (
-            p.whoInvite.find((e) => e.dietitianID === dID) ? (
-              <div className={style["inviting-list"]}>
-                <div className={style.invite}>
-                  <div>
-                    您向 {p.name} {p.gender === "男" ? "先生" : "小姐"}提出邀請
-                  </div>
-                  <div className={style.buttons}>
-                    <button className={style.check}>查看詳情</button>
-                    <button className={style.cancel}>取消邀請</button>
-                  </div>
-                </div>
-              </div>
-            ) : (
-              ""
-            )
-          ) : (
-            ""
-          )
-        )}
+        <div className={style["inviting-list"]}>
+          {publish.map((p, pubIndex) =>
+            p.whoInvite && p.whoInvite.find((e) => e.dietitianID === dID)
+              ? p.whoInvite.map((e, index) =>
+                  e.dietitianID === dID ? (
+                    <>
+                      <div className={style.invite} key={index}>
+                        <div>
+                          您向 {p.name} {p.gender === "男" ? "先生" : "小姐"}
+                          提出邀請
+                        </div>
+                        <div className={style.buttons}>
+                          {p.whoInvite[index].status === "0" ? (
+                            <>
+                              <button
+                                className={style.check}
+                                id={pubIndex}
+                                onClick={checkDetailsHandler}
+                              >
+                                查看詳情
+                              </button>
+                              <button
+                                id={pubIndex}
+                                className={style.cancel}
+                                onClick={cancelInviteHandler}
+                              >
+                                取消邀請
+                              </button>
+                            </>
+                          ) : p.whoInvite[index].status === "1" ? (
+                            <>
+                              <button
+                                className={style.check}
+                                id={pubIndex}
+                                onClick={checkDetailsHandler}
+                              >
+                                查看詳情
+                              </button>
+                              <button className={style.cancel}>成功</button>
+                            </>
+                          ) : p.whoInvite[index].status === "2" ? (
+                            <button className={style.cancel}>婉拒</button>
+                          ) : p.whoInvite[index].status === "3" ? (
+                            <button className={style.cancel}>逾期</button>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                      </div>
+
+                      {parseInt(idx) == pubIndex ? (
+                        <PublicationData
+                          key={p.name}
+                          publish={p}
+                          display={display}
+                          setDisplay={setDisplay}
+                        />
+                      ) : (
+                        ""
+                      )}
+                    </>
+                  ) : (
+                    ""
+                  )
+                )
+              : ""
+          )}
+        </div>
       </div>
     </>
   );
--- light-life/src/Pages/Dietitian/FindCustomers/PublicationData.js
+++ light-life/src/Pages/Dietitian/FindCustomers/PublicationData.js
@@ -14,6 +14,11 @@ import style from "../../../style/findCustomers.module.scss";
 
 function PublicationData({ publish, display, setDisplay }) {
   const [profile, setProfile] = useState({});
+  const [invite, setInvite] = useState({});
+  const { dID } = useParams();
+  const date = new Date(+new Date() + 8 * 3600 * 1000);
+  const today = date.toISOString().substr(0, 10);
+
   useEffect(() => {
     firebase
       .firestore()
@@ -25,10 +30,52 @@ function PublicationData({ publish, display, setDisplay }) {
       });
   }, []);
 
+  const messageHandler = (e) => {
+    setInvite({
+      dietitianID: dID,
+      message: e.target.value,
+      status: "0",
+      inviteDate: today,
+    });
+  };
+
+  const sendMessageHandler = () => {
+    if (publish.whoInvite) {
+      firebase
+        .firestore()
+        .collection("publish")
+        .doc(publish.publishID)
+        .set(
+          {
+            whoInvite: [...publish.whoInvite, invite],
+          },
+          { merge: true }
+        )
+        .then(() => {
+          alert("邀請成功!");
+          window.location.reload();
+        });
+    } else {
+      firebase
+        .firestore()
+        .collection("publish")
+        .doc(publish.publishID)
+        .set(
+          {
+            whoInvite: [invite],
+          },
+          { merge: true }
+        )
+        .then(() => {
+          alert("邀請成功!");
+          window.location.reload();
+        });
+    }
+  };
+
   const closeDetailsHandler = () => {
     setDisplay("none");
   };
-
   return (
     <div className={style.publicationForm} style={{ display: display }}>
       <div>
@@ -40,7 +87,7 @@ function PublicationData({ publish, display, setDisplay }) {
       </div>
 
       <div className={style.flexbox}>
-        <img id="profile-img" src={noImage} alt="customer" />
+        <img id="profile-img" src={profile.image || noImage} alt="customer" />
         <div>
           <div className={style["data-item"]}>
             <div className={style.title}>姓名</div>
@@ -96,14 +143,47 @@ function PublicationData({ publish, display, setDisplay }) {
       </div>
 
       <div className={style.message}>
-        <h3>邀請訊息</h3>
-        <textarea>想為您服務</textarea>
-        <div className={style.buttons}>
-          <button className={style.send}>發送</button>
-          <button className={style.cancel} onClick={closeDetailsHandler}>
-            取消
-          </button>
+        <h3>需求描述</h3>
+        <div style={{ maxHeight: "5rem", overflow: "auto" }}>
+          {publish.content}
         </div>
+      </div>
+
+      <div className={style.message}>
+        <h3>邀請訊息</h3>
+
+        {publish.whoInvite &&
+        publish.whoInvite.find((e) => e.dietitianID === dID) ? (
+          publish.whoInvite.map((e) =>
+            e.dietitianID === dID ? (
+              <div
+                style={{
+                  maxHeight: "5rem",
+                  overflow: "auto",
+                }}
+              >
+                {e.message}
+              </div>
+            ) : (
+              ""
+            )
+          )
+        ) : (
+          <>
+            <textarea
+              value={invite.message ? invite.message : ""}
+              onChange={messageHandler}
+            ></textarea>
+            <div className={style.buttons}>
+              <button className={style.send} onClick={sendMessageHandler}>
+                發送
+              </button>
+              <button className={style.cancel} onClick={closeDetailsHandler}>
+                取消
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
