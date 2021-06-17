f54be451054c5bee6e17baac82b71996b134f06d
NycteaChen <jungturn01tw@gmail.com>
2021-06-17 18:03:18 +0800

    dietaryRecord page: utils/firebae.js

--- light-life/src/utils/Firebase.js
+++ light-life/src/utils/Firebase.js
@@ -15,10 +15,44 @@ export function getDietitiansData() {
   refDietitians.get().then((docs) => {});
 }
 
+export function getMyCustomerData(dID, cID) {
+  return refDietitians.doc(dID).collection("customers").doc(cID).get();
+}
+
 export function getCustomerData(id) {
   return refCustomers.doc(id).get();
 }
 
 export function updateCustomerData(id, update) {
   return refCustomers.doc(id).update(update);
+}
+
+export function getDietData(dID, cID, date) {
+  return refDietitians
+    .doc(dID)
+    .collection("customers")
+    .doc(cID)
+    .collection("diet")
+    .doc(date)
+    .get();
+}
+
+export function updateDietAdvice(dID, cID, date, advice) {
+  return refDietitians
+    .doc(dID)
+    .collection("customers")
+    .doc(cID)
+    .collection("diet")
+    .doc(date)
+    .update(advice);
+}
+
+export function updateCustomerDiet(dID, cID, date, set) {
+  return refDietitians
+    .doc(dID)
+    .collection("customers")
+    .doc(cID)
+    .collection("diet")
+    .doc(date)
+    .set(set, { merge: true });
 }
