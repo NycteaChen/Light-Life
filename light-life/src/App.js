3f625f303ebe38e831aaca4088826cf7a5ed59ed
NycteaChen <jungturn01tw@gmail.com>
2021-06-21 01:48:22 +0800

    add Header.js

--- light-life/src/App.js
+++ light-life/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home.js";
+import Header from "./Pages/Components/Header.js";
 import Dietitian from "./Pages/Dietitian/Dietitian.js";
 import Customer from "./Pages/Customer/Customer.js";
 import NotFound from "./Pages/NotFound/NotFound.js";
-import logo from "./images/lightlife-horizontal.png";
-import style from "./style/basic.module.scss";
 
 function App() {
   return (
@@ -15,34 +14,15 @@ function App() {
           <Home />
         </Route>
         <Route path="/dietitian/:dID">
-          <header className={style["m-header"]}>
-            <div className={style["logo-nav"]}>
-              <a href="/">
-                <img src={logo} id={style.logo} alt="logo" />
-              </a>
-            </div>
-          </header>
+          <Header />
           <Dietitian />
         </Route>
         <Route path="/customer/:cID">
-          <header className={style["m-header"]}>
-            <div className={style["logo-nav"]}>
-              <a href="/">
-                <img src={logo} id={style.logo} alt="logo" />
-              </a>
-              <div id="menu" style={{ width: "24px", height: "24px" }}></div>
-            </div>
-          </header>
+          <Header />
           <Customer />
         </Route>
         <Route>
-          <header className={style["m-header"]}>
-            <div className={style["logo-nav"]}>
-              <a href="/">
-                <img src={logo} id={style.logo} alt="logo" />
-              </a>
-            </div>
-          </header>
+          <Header />
           <NotFound />
         </Route>
       </Switch>
---
+++ light-life/src/Pages/Components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "../../images/lightlife-horizontal.png";
+import style from "../../style/basic.module.scss";
+
+function Header() {
+  return (
+    <header className={style["m-header"]}>
+      <div className={style["logo-nav"]}>
+        <a href="/">
+          <img src={logo} id={style.logo} alt="logo" />
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
