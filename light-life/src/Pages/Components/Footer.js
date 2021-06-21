87f85df327c9e64ad383010dab3a6d9ca9ce587d
NycteaChen <jungturn01tw@gmail.com>
2021-06-21 12:28:54 +0800

    add Footer.js

---
+++ light-life/src/Pages/Components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import style from "../../style/home.module.scss";
+import notFound from "../../style/NotFound.module.scss";
+import { useLocation } from "react-router-dom";
+export default function Footer() {
+  const { pathname } = useLocation();
+  return (
+    <footer className={pathname === "/" ? "" : notFound.footer}>
+      <div>
+        <a
+          href="https://github.com/NycteaChen"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className={`fa fa-github ${style.gitIcon}`} aria-hidden="true"></i>
+        </a>
+        <p>jungturn01tw@gmail.com</p>
+      </div>
+      <p>&copy;2021 Light Life</p>
+    </footer>
+  );
+}
